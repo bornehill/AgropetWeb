364c44c9d816ca5a52842fa13f12f75fc155ed3c
bornehill <ali.escamilla.navarro@gmail.com>
2019-04-09 11:41:58 -0500

    First commit, Add TableView

--- Agropet/Index.aspx.cs
+++ Agropet/Index.aspx.cs
@@ -5,37 +5,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using PetData.Pet;
+using Agropet.Entidades.Seguridad;
+using Agropet.Entidades.Especial;
 
 namespace Agropet.website
 {
-    public partial class Index : System.Web.UI.Page
+  public partial class Index : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
     {
-      protected void Page_Load(object sender, EventArgs e)
-      {
 
-      }
+    }
 
-      protected void GetMenuWeb() {
-        PetService service = new PetService();
-        var menus = service.GetMenuWeb(0, "");
-        foreach (var menu in menus) {
-          var submenu = service.GetMenuWeb(menu.MenuId, menu.Menu);
-          if (submenu.Count > 0)
-          {
-            Response.Write("<li class=\"dropdown\">");
-            Response.Write("<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\" "+menu.MenuId.ToString()+" \">"+menu.Menu+"<span class=\"caret\"></span></a>");
-            Response.Write("<ul class=\"dropdown-menu\">");
-            foreach (var sub in submenu)
-            {
-              Response.Write("<li><a href=\" "+sub.MenuId+" \">"+sub.Menu+"</a></li>");
-            }
-            Response.Write("</ul>");
-            Response.Write("</li>");
-          }
-          else {
-            Response.Write("<li><a href=\" "+menu.MenuId+" \">"+menu.Menu+"</a></li>");
-          }
-        }
+    protected void GetBannersWeb() {
+      (this.Master as MasterPet).GetBannersWeb();
     }
   }
 }
